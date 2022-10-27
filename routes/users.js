890cb8cc317a2b5e0808ab03f4d7feb6c6a39bb0
Istatillo <istatillojorayev@gmail.com>
2022-10-27 17:02:55 +0500

    changed status

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,20 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const users = await User.find({});
+  res.status(200);
   res.send(users);
 });
 
 router.post("/", async (req, res) => {
   const { name, age } = req.body;
   await User.create({ name, age });
-
+  res.status(201);
   res.send("Malumot saqlandi!");
 });
 
 router.delete("/:id", async (req, res) => {
   await User.findByIdAndDelete(req.params.id);
+  res.status(200);
   res.send("O'chirildi!");
 });
 
@@ -24,7 +26,7 @@ router.put("/", async (req, res) => {
   const { name, age, id } = req.body;
 
   await User.findByIdAndUpdate(id, { name, age });
-
+  res.status(205);
   res.send("O'zgartirildi!");
 });
 
